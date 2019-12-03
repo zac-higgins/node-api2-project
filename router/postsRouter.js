0a01fb2bdcc3497ff69eeca40d06353474dba09d
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-03 16:09:29 -0700

    added endpoint to list all comments on a given post. Added comments to explain each endpoint

--- router/postsRouter.js
+++ router/postsRouter.js
@@ -6,6 +6,7 @@ router.use(express.json());
 
 // -------------- GET requests ------------------- //
 
+//returns a list of all posts
 router.get('/', (req, res) => {
     db.find(req.query)
         .then(posts => {
@@ -18,6 +19,7 @@ router.get('/', (req, res) => {
         })
 })
 
+//returns a post by its ID
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     db.findById(id)
@@ -34,6 +36,24 @@ router.get('/:id', (req, res) => {
             res.status(500).json({ error: "The post information could not be retrieved." });
         });
 
+})
+
+//returns all of the comments for a given post, the post is found by its ID
+router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+    db.findPostComments(id)
+        .then(comment => {
+            if (comment.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(comment)
+            }
+        })
+        .catch(error => {
+            // log error to database
+            console.log(error);
+            res.status(500).json({ error: "The comments information could not be retrieved." });
+        });
 })
 
 module.exports = router;
