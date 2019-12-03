930f6d78614db6939075101ea1a89a0c73e0f50d
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-03 16:31:20 -0700

    created endpoint to create a new post

--- router/postsRouter.js
+++ router/postsRouter.js
@@ -6,7 +6,7 @@ router.use(express.json());
 
 // -------------- GET requests ------------------- //
 
-//returns a list of all posts
+//Returns an array of all the post objects contained in the database.
 router.get('/', (req, res) => {
     db.find(req.query)
         .then(posts => {
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
         })
 })
 
-//returns a post by its ID
+//Returns the post object with the specified id.
 router.get('/:id', (req, res) => {
     const id = req.params.id;
     db.findById(id)
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
 
 })
 
-//returns all of the comments for a given post, the post is found by its ID
+//Returns an array of all the comment objects associated with the post with the specified id.
 router.get('/:id/comments', (req, res) => {
     const id = req.params.id;
     db.findPostComments(id)
@@ -55,5 +55,39 @@ router.get('/:id/comments', (req, res) => {
             res.status(500).json({ error: "The comments information could not be retrieved." });
         });
 })
+
+// -------------- POST requests ------------------- //
+
+//Creates a post using the information sent inside the request body.
+router.post('/', (req, res) => {
+    postData = req.body;
+
+    if (!postData.title || !postData.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        db.insert(postData)
+            .then(post => {
+                res.status(201).json({ postData })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+            })
+
+    }
+});
+
+//Creates a comment for the post with the specified id using information sent inside of the request body.
+router.post('/:id/comments', (req, res) => {
+
+});
+
+// -------------- PUT requests ------------------- //
+
+//Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+
+// -------------- DELETE requests ------------------- //
+
+//Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
 
 module.exports = router;
