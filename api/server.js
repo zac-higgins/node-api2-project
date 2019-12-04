e6d2d4cd08dddfaa9159bb679008f642c68980ce
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-04 12:19:06 -0700

    changed sanity check success message to state which project is running

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const postsRouter = require('../router/postsRouter');
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send('It\'s working!!');
+    res.send('Project: node-api2-project is up and running!');
 });
 
 server.use('/api/posts', postsRouter);
