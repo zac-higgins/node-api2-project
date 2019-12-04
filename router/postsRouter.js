fef399127a1b87c9b90f77d2686ea773111eb04c
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-03 17:51:00 -0700

    created endpoint that allows a put request to update a post

--- router/postsRouter.js
+++ router/postsRouter.js
@@ -85,6 +85,42 @@ router.post('/:id/comments', (req, res) => {
 // -------------- PUT requests ------------------- //
 
 //Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const postData = req.body;
+
+    if (!postData.title || !postData.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        db.findById(id)
+            .then(post => {
+                if (post.length === 0) {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                } else {
+                    db.update(id, postData)
+                        .then(() => {
+                            db.findById(id)
+                                .then(post => {
+                                    res.status(200).json(post)
+                                })
+                                .catch(err => {
+                                    console.log(`error on GET /api/posts/${id}`, err);
+                                    res.status(500).json({ error: "The post information could not be retrieved." })
+                                })
+                        })
+                        .catch(err => {
+                            console.log('error on PUT /api/posts/:id', err);
+                            res.status(500).json({ error: "The post information could not be modified." })
+                        })
+                }
+            })
+            .catch(error => {
+                // log error to database
+                console.log(error);
+                res.status(500).json({ error: "The post information could not be retrieved." });
+            });
+    }
+})
 
 // -------------- DELETE requests ------------------- //
 
